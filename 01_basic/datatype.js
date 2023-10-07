4d6f1ca8e7e9ea6e57d98ccacbb9cd21907161f5
Ankitbettiah <143796193+Ankitbettiah@users.noreply.github.com>
2023-10-07 12:12:03 +0000

    variables and datatype of js

---
+++ 01_basic/datatype.js
@@ -0,0 +1,4 @@
+let an=null
+console.log(typeof(an))
+//null has data type object .some people tell is a languese problam
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,14 @@
+const AccoundId=123455
+
+var cardNo=13467     
+//Do  not var keyword for initialing  varible because it has Scope problam
+
+let AccountHolder="Ankit Raj"
+
+age =23
+//This is not good pratices to use or initize varibles without let keyword
+
+console.log(AccountHolder)
+console.table([AccoundId,cardNo,AccountHolder,age])
+//console table is used to print attributes in table formate 
+
