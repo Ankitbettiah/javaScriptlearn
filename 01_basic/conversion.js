6c242dafebf1559ca94dba0ea8ca63c7e5a3fa1b
Ankitbettiah <143796193+Ankitbettiah@users.noreply.github.com>
2023-10-07 12:28:46 +0000

    conversion in js

---
+++ 01_basic/conversion.js
@@ -0,0 +1,12 @@
+let a="33"
+
+let IntiNum=Number(a)
+
+let ab="33abn"  // it also convert it into numbers 
+let test2=Number(ab)
+console.log(test2)//we will get NaN as an output it means that shows gitchs 
+
+let pa=null
+let h=Number(null)
+console.log(h)
+//Here we show that null is convered into 0.
